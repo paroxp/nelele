c4942a54cfbe8a3895047e2a1de4a516a8ec7359
Rafal Proszowski <paroxp@gmail.com>
2025-11-29 22:03:45 +0000

    Revert "layout: hack fixed background for iOS"
    This reverts commit f999eeba63019088421bbacbfbcf1a28fc2730fe.
    
    wow...

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -114,7 +114,6 @@ export function htmlDocument(config: Config, body: string): string {
     </head>
 
     <body>
-      <div class="page background"></div>
       <div class="language-selection">
         <a data-language="${langSelector.title}" href="${path.join(langSelector.path, (config.path || '').replace(`/${language}`, ''))}" title="${langSelector.title}"></a>
       </div>
